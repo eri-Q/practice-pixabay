943f0e8dd9a015036104223c35642ed741ea4f21
EriHatanaka <eri.hatanaka8@gmail.com>
2023-02-26 18:13:02 +0900

    Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
+import logo from "./logo.png";
+
 const App = () => {
-  const onSearchSubmit = (term) => {
-    console.log(term);
+  const [images, setImages] = useState([]);
+  const ApiKey = process.env.REACT_APP_PIXABAY_APIKEY;
+  const onSearchSubmit = async (term) => {
+    try {
+      const params = {
+        key: ApiKey,
+        q: term,
+      };
+      const response = await axios.get("https://pixabay.com/api", { params });
+      setImages(response.data.hits);
+      if (response.data.total === 0) {
+        window.alert("Not Found");
+      }
+    } catch {
+      window.alert("Fail to find photos");
+    }
   };
 
   return (
     <div className="ui container" style={{ marginTop: "20px" }}>
+      <img src={logo} alt="pixabay-logo" className="pixabay-logo" />
       <SearchBar onSubmit={onSearchSubmit} />
+      <ImageList images={images} />
     </div>
   );
 };
